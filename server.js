97d65d91dc4cffefd6d44a17014100eb82bf2d57
Nh4nisme <nguyenhuunhan190495@gmail.com>
2025-04-07 14:39:00 +0700

    frontend done, progress backend

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("✅ MongoDB connected");
+    console.log("MongoDB connected");
 }).catch(err => {
-    console.error("❌ MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err);
 });
 
 // Middleware
